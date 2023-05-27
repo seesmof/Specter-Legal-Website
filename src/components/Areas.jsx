1223cc5757ec34d29ba784559a76689700233f83
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-27 20:44:56 +0300

    modified Areas.jsx and index.css

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const Areas = () => {
   return (
     <div>
-      <h2 className="heading">Areas of practice</h2>
+      <div className="flex flex-col items-center p-4 pt-16 text-center">
+        <h2 className="text-3xl">Areas of practice</h2>
+        <p className="py-6 text-lg">
+          Nullam tincidunt adipiscing enim. Praesent egestas neques eu enim.
+          Vivamus elementum semper nisi. Etiam rhoncus.
+        </p>
+        <a href="#" className="button px-8">
+          See all services
+        </a>
+      </div>
     </div>
   );
 };
