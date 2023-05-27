bdbeaa689471d66145fb6921b213200c7e952021
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-27 19:56:54 +0300

    modified App.jsx added Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const Page = () => {
   return (
-    <div className="bg-background text-accent scroll-smooth w-full min-h-screen">
+    <div className="min-h-screen w-full scroll-smooth bg-background text-accent">
       <Header />
+      <Hero />
     </div>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../index.css";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col py-12 pb-6">
+      <h1 className="text-5xl">
+        Offering the highest quality{" "}
+        <span className="italic">legal services</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
