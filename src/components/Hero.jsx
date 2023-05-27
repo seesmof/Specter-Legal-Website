ec99a14ca66ed0313ec09e445ecb0acf4b0dcf34
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-27 20:38:11 +0300

    modified Hero.jsx in components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,25 @@ import "../index.css";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center p-4 py-16 pb-6">
-      <h1 className="px-4 text-center text-5xl font-[400] leading-[1.35]">
-        Offering the highest quality{" "}
-        <span className="font-[300] italic">legal services</span>
-      </h1>
-      <p className="py-8 text-center text-lg">
-        Through 10 years of experience and over 200 million in transaction
-        values we offer the most premium legal services.
-      </p>
-      <a href="#" className="button mt-2 scale-[1.35] px-8 py-3">
-        Let's talk
-      </a>
+    <div className="">
+      <div className="flex flex-col items-center justify-center p-4 pt-16">
+        <h1 className="px-4 text-center text-5xl font-[400] leading-[1.35]">
+          Offering the highest quality{" "}
+          <span className="font-[300] italic">legal services</span>
+        </h1>
+        <p className="py-8 text-center text-lg">
+          Through 10 years of experience and over 200 million in transaction
+          values we offer the most premium legal services.
+        </p>
+        <a href="#" className="button mt-2 scale-[1.35] px-8 py-3">
+          Let's talk
+        </a>
+      </div>
 
-      <div className="relative flex h-20 w-full items-center justify-center bg-accent"></div>
+      <div className="relative mt-14 flex w-full items-center justify-center">
+        <img src="/hero_image.webp" alt="" className="z-20 scale-90" />
+        <div className="absolute z-10 h-2/3 w-full bg-accent"></div>
+      </div>
     </div>
   );
 };
