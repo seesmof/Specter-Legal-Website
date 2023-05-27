e7ffe6a05921b1bd54dccd5554f02f2cd25db2a7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-27 20:22:39 +0300

    modified Hero.jsx in components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,18 @@ import "../index.css";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col py-12 pb-6">
-      <h1 className="px-12 text-center text-5xl font-[400] leading-[1.35]">
+    <div className="flex flex-col items-center justify-center p-4 py-12 pb-6">
+      <h1 className="px-4 text-center text-5xl font-[400] leading-[1.35]">
         Offering the highest quality{" "}
         <span className="font-[300] italic">legal services</span>
       </h1>
+      <p className="py-8 text-center text-lg">
+        Through 10 years of experience and over 200 million in transaction
+        values we offer the most premium legal services.
+      </p>
+      <a href="#" className="button scale-[1.35] px-5">
+        Let's talk
+      </a>
     </div>
   );
 };
