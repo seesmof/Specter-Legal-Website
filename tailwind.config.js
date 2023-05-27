fe9dec1a08ddb78506870e8c2fa20d8255ecdf95
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-27 16:21:32 +0300

    added 11 files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./**/*.{jsx,js,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+  base: "/specter-ross-legal-company-landing/",
+  server: {
+    port: 3000,
+  },
+});
