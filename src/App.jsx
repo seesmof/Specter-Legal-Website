2bbe589f4043315e85e067b931475c7b8cd11ccc
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-27 20:39:00 +0300

    modified App.jsx added Areas.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Areas from "./components/Areas";
 
 const Page = () => {
   return (
     <div className="min-h-screen w-full scroll-smooth bg-background text-accent">
       <Header />
       <Hero />
+      <Areas />
     </div>
   );
 };
