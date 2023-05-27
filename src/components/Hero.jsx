36c590630d641cb39aca849ce7065122447025b4
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-27 20:19:53 +0300

    modified index.html and Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import "../index.css";
 const Hero = () => {
   return (
     <div className="flex flex-col py-12 pb-6">
-      <h1 className="text-5xl">
+      <h1 className="px-12 text-center text-5xl font-[400] leading-[1.35]">
         Offering the highest quality{" "}
-        <span className="italic">legal services</span>
+        <span className="font-[300] italic">legal services</span>
       </h1>
     </div>
   );
